06a31a37e45422f44d96dabbfad5ec45bbe5783e
MarinBrkic <152974525+MarinBrkic@users.noreply.github.com>
2023-12-12 12:14:22 +0100

    Add files via upload

---
+++ varijable_ispis_podataka.cs
@@ -0,0 +1,13 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int a = 100;
+		int b = 3;
+		int x = a/b;
+		int y = 100 % 3;
+		Console.WriteLine("{0} podijeljeno sa {1} je {2}, i ostatak je {3}.", a, b, x, y);
+	}
+}
